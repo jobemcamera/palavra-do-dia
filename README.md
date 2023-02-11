a8aa268da81e4ae5f47172f5184647630de1dbd1
jobemcamera <jobecamera@gmail.com>
2023-02-10 21:16:44 -0300

    Adicionar as funções para as 6 tentativas. Data attributes

--- README.md
+++ README.md
@@ -4,16 +4,18 @@ Projeto inspirado no jogo [Palavra do Dia](https://palavra-do-dia.pt/)
 
 ## Desafios
 
-- (X) Layout inicial da página;
-- (X) Criar um array de palavras com 5 letras;
-- (X) Sortear uma palavra aleatoriamente;
-- (X) Receber e concatenar letras do input em um array;
-- (X) Tratar inputs para receber somente letras;
-- (X) Pular para o próximo input após preencher o anterior;
-- (X) Apagar letra do input anterior com a tecla Backspace;
-- (X) Verificar se a letra na posição informada é igual a letra na posição da palavra do dia;
-    - (X) Se letra está na posição certa: mudar cor do input para verde;
-    - (X) Se letra está na posição errada mas existe na palavra do dia: mudar cor do input para amarelo;
-    - (X) Se letra não existe na palavra do dia: mudar cor do input para vermelho;
-- (X) Animação dos inputs após dar enter nas letras informadas;
-- ( ) Após 6 palavras erradas, o jogo acaba e é mostrado a palavra do dia na tela;
+- [X] Layout inicial da página;
+- [X] Criar um array de palavras com 5 letras;
+- [X] Sortear uma palavra aleatoriamente;
+- [X] Receber e concatenar letras do input em um array;
+- [X] Tratar inputs para receber somente letras;
+- [X] Pular para o próximo input após preencher o anterior;
+- [X] Apagar letra do input anterior com a tecla Backspace;
+- [X] Verificar se a letra na posição informada é igual a letra na posição da palavra do dia;
+    - [X] Se letra está na posição certa: mudar cor do input para verde;
+    - [X] Se letra está na posição errada mas existe na palavra do dia: mudar cor do input para amarelo;
+    - [X] Se letra não existe na palavra do dia: mudar cor do input para vermelho;
+- [X] Animação dos inputs após dar enter nas letras informadas;
+- [X] Adicionar as funções para as 6 tentativas;
+- [ ] Após tentar a palavra, mudar cursor para a próxima tentativa;
+- [ ] Após 6 palavras erradas, o jogo acaba e é mostrado a palavra do dia na tela;
--- assets/app/main.js
+++ assets/app/main.js
@@ -1,40 +1,46 @@
 import { sortearPalavra } from "./listaDePalavras.js";
-//import { listaDePalavras } from "./listaDePalavras.js";
 
-const letraDigitada = document.querySelectorAll('.primeira-palavra');
 let palavraFormada = [];
 let listaLetras = [];
 const palavraSorteada = sortearPalavra();
+let ordem = 1;
 
-// Evento de keypress do botão ENTER
-document.addEventListener("keypress", function(evento) {
-    if (evento.key == 'Enter') {
-        letraDigitada.forEach((letra, index) => {
-            if (!letra.value == "") {
-                console.log(`index ${index} letra ${letra.value}`);
-                listaLetras.push(letra.value);
-                if (listaLetras[index] == palavraSorteada[index]) {
-                    letraDigitada[index].classList.add('certo');
-                } else if (palavraSorteada.includes(listaLetras[index])) {
-                    letraDigitada[index].classList.add('errado');
-                } else {
-                    letraDigitada[index].classList.add('ausente');
-                }
-                
+function ordemDaTentativaDaPalavra() {
+    
+    const ordemDaPalavra = document.querySelectorAll(`[data-palavra="${ordem}"]`);
+    ordemDaPalavra.forEach((letra, index) => {
+        if (!letra.value == "") {
+            console.log(`index ${index} letra ${letra.value}`);
+            listaLetras.push(letra.value);
+            if (listaLetras[index] == palavraSorteada[index]) {
+                ordemDaPalavra[index].classList.add('certo');
+            } else if (palavraSorteada.includes(listaLetras[index])) {
+                ordemDaPalavra[index].classList.add('errado');
             } else {
-                console.log(`index ${index} vazio`);
+                ordemDaPalavra[index].classList.add('ausente');
             }
-                        
-        });
-        palavraFormada = listaLetras.join('');
-        console.log(`\n A palavra foramda é: ${palavraFormada}`);
-        if (palavraFormada == palavraSorteada) {
-            console.log('Você acertou!');
         } else {
-            console.log('Você errou!');
-        }        
-    }
+            console.log(`index ${index} vazio`);
+        }
+                    
+    });
+
+    palavraFormada = listaLetras.join('');
+    console.log(`\n A palavra foramda é: ${palavraFormada}`);
+
+    if (palavraFormada == palavraSorteada) {
+        console.log('Você acertou!');
+    } else {
+        console.log('Você errou!');
+    }     
+
     palavraFormada = [];
     listaLetras = [];
-});
+    ordem += 1;
+}
 
+document.addEventListener("keypress", function(evento) {
+    if (evento.key == 'Enter') {
+        ordemDaTentativaDaPalavra();
+    }
+});
