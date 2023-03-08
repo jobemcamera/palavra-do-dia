1c751d00a81975461a8011aa80f02aa89621b071
jobemcamera <jobecamera@gmail.com>
2023-03-07 21:56:11 -0300

    reorganização do modal duvidas e add modal estatísticas

--- assets/app/modal.js
+++ assets/app/modal.js
@@ -1,12 +1,30 @@
+// -------------------------- MODAL DÚVIDAS --------------------------
+
 const abrirModalDuvidas = document.querySelector("#botao-modal-duvidas");
 const modalDuvidas = document.querySelector("#modal-duvidas");
 const opacidade = document.querySelector("#opacidade");
+const fecharModalDuvidas = document.querySelector("#botao-fechar-duvidas");
 
-function mudaOpacidade() {
+[abrirModalDuvidas, fecharModalDuvidas].forEach(elemento => {
+    elemento.addEventListener("click", abreModalDuvidas);
+});
+
+function abreModalDuvidas() {
     modalDuvidas.classList.toggle("escondido");
     opacidade.classList.toggle("escondido");
 }
 
-[abrirModalDuvidas, modalDuvidas, opacidade].forEach(elemento => {
-    elemento.addEventListener("click", mudaOpacidade);
-})
+// ------------------------ MODAL ESTATÍSTICAS ------------------------
+
+const abrirModalEstatisticas = document.querySelector("#botao-modal-estatisticas");
+const modalEstatisticas = document.querySelector("#modal-estatisticas");
+const fecharModalEstatisticas = document.querySelector("#botao-fechar-estatisticas");
+
+[abrirModalEstatisticas, fecharModalEstatisticas].forEach(elemento => {
+    elemento.addEventListener("click", abreModalEstatisticas);
+});
+
+function abreModalEstatisticas() {
+    modalEstatisticas.classList.toggle("escondido");
+    opacidade.classList.toggle("escondido");
+}
